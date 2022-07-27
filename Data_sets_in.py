3c7a5c144cf37a1e9b252e180a2571c9f1b5058c
MariaCz <maria.czapczyk@gmail.com>
2022-07-27 14:02:01 +0200

    end course week2 commit

--- Data_sets_in.py
+++ Data_sets_in.py
@@ -1,3 +1,8 @@
+from curses.ascii import isupper
+import random
+from re import X
+import sys
+
 # LISTS - MUTABLE DATA TYPES
 
 # konstruktor listy zwraca None
@@ -40,5 +45,77 @@ print(color_list)
 #zwraca index danego itemu
 print(numbers.index(6))
 
-                
 
+#list comprehensions przykłady:
+lista = [x*2 for x in (3,2,4,5,6)]
+print(lista)
+
+#(stworz liste dla wartosci randomowych z przedzialow: (0,10)...(1,10)...(4,10)...(9,10))
+scores = [random.randint(i,10) for i in range(10)]
+print(scores)
+
+#(przyklad nested conditional:)
+caps = []
+
+for letter in 'Henry Honey':
+    if letter.isupper():
+        caps.append(letter)
+print(caps)
+
+caps = [letter for letter in 'Henry Honey' if letter.isupper()]
+print(caps)
+
+#flat list:
+list_of_lists = [['a','b','c'],['d','e','f']]
+flat = []
+
+for sub_list in list_of_lists:
+    for item in sub_list:
+        flat.append(item)
+        
+print(flat)
+
+flat = [item for sub_list in list_of_lists for item in sub_list]
+print(flat)
+# sprawdzanie rozmiaru zmiennej (bajty) 
+print(sys.getsizeof(flat))
+
+#GENERATOR EXPRESSIONS ()
+
+flat = (item for sub_list in list_of_lists for item in sub_list)
+print(sys.getsizeof(flat))
+
+#get size of:
+print(sys.getsizeof([score+1 for score in range(10)]))
+print(sys.getsizeof((score+1 for score in range(10))))
+print(flat)
+
+#GENERATOR FUNCTIONS
+
+def counter(x):
+    while x<50:
+        yield x
+        x+=1
+        
+get_number = counter(12)
+
+cycle = [count for count in get_number]
+print(cycle)
+
+#
+def foo_moo(i):
+    while True:
+        if i%5 ==0:
+            yield 'foo'
+            print(i)
+            i += 1
+      
+        else:
+            yield 'moo'
+            print(i)
+            i += 1
+            
+fm = foo_moo(5)
+
+for _ in range(11):
+    print(next(fm))
